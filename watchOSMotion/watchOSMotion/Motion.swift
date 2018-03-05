8a0793cdc64cc27f1b7bb929aa6e63a4f1a3c30d
jagaa <jagaa1103@gmail.com>
2018-03-05 18:35:32 +0900

    init

---
+++ watchOSMotion/watchOSMotion/Motion.swift
@@ -0,0 +1,14 @@
+//
+//  Motion.swift
+//  watchOSMotion
+//
+//  Created by Enkhjargal Gansukh on 05/03/2018.
+//  Copyright © 2018 Enkhjargal Gansukh. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+class Motion{
+    
+}
---
+++ watchOSMotion/watchOSMotion/watchOSMotion.h
@@ -0,0 +1,19 @@
+//
+//  watchOSMotion.h
+//  watchOSMotion
+//
+//  Created by Enkhjargal Gansukh on 05/03/2018.
+//  Copyright © 2018 Enkhjargal Gansukh. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for watchOSMotion.
+FOUNDATION_EXPORT double watchOSMotionVersionNumber;
+
+//! Project version string for watchOSMotion.
+FOUNDATION_EXPORT const unsigned char watchOSMotionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <watchOSMotion/PublicHeader.h>
+
+
